17d76fe19587f7278c68c40e0900e66795b457a4
Supriyo-Brandstory <supriyo@brandtory.in>
2025-09-16 15:38:22 +0530

    ..

--- src/middleware.js
+++ src/middleware.js
@@ -6,13 +6,13 @@ export function middleware(req) {
   const host = req.headers.get("host");
 
   // Force www
-  if (host && host.startsWith("example.com")) {
+  if (host && host.startsWith("websitedevelopmentagency.ae")) {
     url.host = `www.${host}`;
     return NextResponse.redirect(url, 301);
   }
 
   // OR force non-www
-  if (host && host.startsWith("www.example.com")) {
+  if (host && host.startsWith("www.websitedevelopmentagency.ae")) {
     url.host = host.replace("www.", "");
     return NextResponse.redirect(url, 301);
   }
