ab333ec0ba636e3bb5b3fe423daa09b32733b585
Supriyo-Brandstory <supriyo@brandtory.in>
2025-09-22 12:13:59 +0530

    ..

--- src/app/(frontend)/(pages)/(home)/page.jsx
+++ src/app/(frontend)/(pages)/(home)/page.jsx
@@ -18,7 +18,7 @@ const page = () => {
   return (
     <div>
         <Banner/>
-        <Trusted/>
+        {/* <Trusted/> */}
         <BestAgency/>
         <WhatSetsUs/>
         <OurCoreStrengths/>
