a1580b1a4a5833de67d29bd675da1282cf80f663
Supriyo-Brandstory <supriyo@brandtory.in>
2026-01-09 19:58:22 +0530

    ....

--- src/app/(frontend)/component/pages/common/CommonAboutPage.jsx
+++ src/app/(frontend)/component/pages/common/CommonAboutPage.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import styles from '@/app/(frontend)/assets/style/common/commonAboutPage.module.css'
-import Image from 'next/image'
+import React from "react";
+import styles from "@/app/(frontend)/assets/style/common/commonAboutPage.module.css";
+import Image from "next/image";
 
-const CommonAboutPage = ({text1,text2,text3,img}) => {
+const CommonAboutPage = ({ text1, text2, text3, img }) => {
   return (
-        <div className='frame-1200 py-100 sm-pt-50 sm-pb-50 sm-px-20'>
-            <div className={styles.flex} style={{'--width': '55%'}}>
-                <div className={styles.left}>
-                    <h2>{text1}</h2>
-                    <p className='my-20' dangerouslySetInnerHTML={{__html:text2}}></p>
-                    <p dangerouslySetInnerHTML={{__html:text3}}></p>
-                </div>
-                <div className={styles.right} style={{'--width': '45%'}}>
-                    <Image src={img} alt="sharjah" style={{'--height': '391px', '--width': '628px'}} />
-
-                </div>
-
-            </div>
+    <div className="frame-1200 py-100 sm-pt-50 sm-pb-50 sm-px-20">
+      <div className={styles.flex}>
+        <div className={styles.left} style={{ "--width": "55%" }}>
+          <h2>{text1}</h2>
+          <p className="my-20" dangerouslySetInnerHTML={{ __html: text2 }}></p>
+          <p dangerouslySetInnerHTML={{ __html: text3 }}></p>
         </div>
-  )
-}
+        <div className={styles.right} style={{ "--width": "45%" }}>
+          <Image
+            src={img}
+            alt="sharjah"
+            style={{ "--height": "391px", "--width": "628px" }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CommonAboutPage
+export default CommonAboutPage;
