46bf5b44a59a193a63cc18fb9b66a0ad479e686e
Supriyo-Brandstory <supriyo@brandtory.in>
2026-01-17 20:08:30 +0530

    ..

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     allowedDevOrigins: ['192.168.0.105', 'localhost:3000'],
+    experimental: {
+        turbo: false,
+    },
 };
 
 export default nextConfig;
