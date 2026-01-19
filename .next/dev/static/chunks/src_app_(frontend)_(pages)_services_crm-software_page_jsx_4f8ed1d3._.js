857b0858a1a429a016ee9b6ffbb16e14ccf1b740
Supriyo-Brandstory <supriyo@brandtory.in>
2026-01-19 18:14:59 +0530

    ..

--- .next/dev/static/chunks/src_app_(frontend)_(pages)_services_crm-software_page_jsx_4f8ed1d3._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_(frontend)_assets_style_029f504f._.css",
-  "static/chunks/src_app_(frontend)_b69dec43._.js",
-  "static/chunks/node_modules_cd88caba._.js"
-],
-    source: "dynamic"
-});
